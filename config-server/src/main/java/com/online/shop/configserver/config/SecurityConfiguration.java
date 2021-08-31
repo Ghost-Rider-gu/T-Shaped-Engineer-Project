9dca7dc699ffbb6f2aed635e09da6c37118ad615
Iurii <ghostrider.gu@gmail.com>
2021-08-31 12:40:59 +0300

    updated config-server and auuth-service

---
+++ config-server/src/main/java/com/online/shop/configserver/config/SecurityConfiguration.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2021 Smart-shop
+ * All rights reserved
+ */
+
+package com.online.shop.configserver.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf()
+                .disable();
+        http
+                .authorizeRequests()
+                .antMatchers("/actuator/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic();
+    }
+}
