47d44bf0df1c039e148fb7642ab53681a9c3309e
Iurii <ghostrider.gu@gmail.com>
2021-09-04 19:44:27 +0300

    added api gateway service

---
+++ api-gateway-service/src/main/java/com/online/shop/gateway/ApiGatewayApplication.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2021 Smart-shop
+ * All rights reserved
+ */
+
+package com.online.shop.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableZuulProxy
+public class ApiGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class, args);
+    }
+}
